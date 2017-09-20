98fd28e61fbf8e5d089eedb1b04b8d7ff5ea5505
Axel Fregler <axel_fregler@hotmail.com>
2017-09-20 15:32:51 +0200

    Added dice and accuracy script

---
+++ XCOM2 Prototype/Assets/Scenes/testScenes/RandomRange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    //int diceRolls;
+    int n;
+    public int numberOfSides;
+    public int numberOfDice;
+
+    void Start () {
+        /*
+        diceRolls = 0;
+        while(true)
+        {
+            n = n + Random.Range(1, numberOfSides);
+            diceRolls++;
+            if (diceRolls == numberOfDice)
+            {
+                break;
+            }
+        }   */
+        for (int i = 0; i < numberOfDice; i++)
+        {
+            n = n + Random.Range(1, numberOfSides);
+        }
+        
+        print(n);
+	}
+	
+	
+}
