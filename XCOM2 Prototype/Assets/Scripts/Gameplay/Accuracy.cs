557629380c720f8b9cb60a5520d4f7f556efbfe0
Axel Fregler <axel_fregler@hotmail.com>
2017-09-25 14:06:54 +0200

    Started creating a game manager.

--- XCOM2 Prototype/Assets/Scripts/Gameplay/Accuracy.cs
+++ XCOM2 Prototype/Assets/Scripts/Gameplay/Accuracy.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Accuracy : MonoBehaviour {
+public class Accuracy : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
