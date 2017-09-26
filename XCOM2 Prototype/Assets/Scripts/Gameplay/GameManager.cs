1db7355b84e6555be926927efc400d260de476e2
JoeSnow0 <thomas.c.richards1@gmail.com>
2017-09-26 09:30:55 +0200

    added buttons to gameSession for abilities. fixed compile error, gameManager had wrong name.

--- XCOM2 Prototype/Assets/Scripts/Gameplay/GameManager.cs
+++ XCOM2 Prototype/Assets/Scripts/Gameplay/GameManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
 
     public List<MonoBehaviour> eventSubscribedScripts = new List<MonoBehaviour>();
