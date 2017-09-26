e8c0c4e6fef5216b9c4ef7860c692811959e4667
JoeSnow0 <thomas.c.richards1@gmail.com>
2017-09-26 13:48:27 +0200

    Replaced Tooltip attribute with Range.

--- XCOM2 Prototype/Assets/Scripts/Gameplay/Health.cs
+++ XCOM2 Prototype/Assets/Scripts/Gameplay/Health.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour {
-    [Tooltip("Health value between 0 and 100.")]
 
     // Use this for initialization
-    public int health = 100;
-    int MAX_HEALTH = 100;
-    int MIN_HEALTH = 0;
+   [Range(0, 100)]
+   public int health = 100;
+   int MAX_HEALTH = 100;
+   int MIN_HEALTH = 0;
     
 
 	void Start () {
