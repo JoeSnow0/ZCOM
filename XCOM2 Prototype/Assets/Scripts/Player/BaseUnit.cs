5815a3df06fce8fa1e3b11d611b69e045b0757e6
Erik Konkell <konkell.erik@gmail.com>
2017-09-27 14:46:42 +0200

    trying to resolve an ishu

--- XCOM2 Prototype/Assets/Scripts/Player/BaseUnit.cs
+++ XCOM2 Prototype/Assets/Scripts/Player/BaseUnit.cs
@@ -6,6 +6,22 @@ public class BaseUnit : MonoBehaviour {
 
     public int tileX;
     public int tileY;
+    public TileMap map;
     public List<TileMap.Node> currentPath = null;
 
+    private void Update()
+    {
+        if (currentPath != null)
+        {
+            int currNode = 0;
+            while (currNode < currentPath.Count - 1)
+            {
+                Vector3 start = map.TileCoordToWorldCoord(currentPath[currNode].x,currentPath[currNode].y);
+                Vector3 end = map.TileCoordToWorldCoord(currentPath[currNode+1].x, currentPath[currNode+1].y);
+                Debug.DrawLine(start, end);
+                currNode++;
+            }
+
+        }
+    }
 }
