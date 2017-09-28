62d3971499fb23fdcd4f77e1c11dd7503f2dc555
Erik Konkell <konkell.erik@gmail.com>
2017-09-28 14:53:28 +0200

    The pathfinding is here!

--- XCOM2 Prototype/Assets/Scripts/Grid-pathing/TileType.cs
+++ XCOM2 Prototype/Assets/Scripts/Grid-pathing/TileType.cs
@@ -7,5 +7,6 @@ public class TileType {
 
     public string name;
     public GameObject tileVisualPrefab;
-
+    public float movemontCost = 10;
+    public bool isWalkeble = true;
 }
--- XCOM2 Prototype/Assets/Scripts/Player/BaseUnit.cs
+++ XCOM2 Prototype/Assets/Scripts/Player/BaseUnit.cs
@@ -7,8 +7,9 @@ public class BaseUnit : MonoBehaviour {
     public int tileX;
     public int tileY;
     public TileMap map;
-    public List<TileMap.Node> currentPath = null;
+    public List<Node> currentPath = null;
 
+    int moveSpeed = 6;
     private void Update()
     {
         if (currentPath != null)
@@ -17,11 +18,38 @@ public class BaseUnit : MonoBehaviour {
             while (currNode < currentPath.Count - 1)
             {
                 Vector3 start = map.TileCoordToWorldCoord(currentPath[currNode].x,currentPath[currNode].y);
-                Vector3 end = map.TileCoordToWorldCoord(currentPath[currNode+1].x, currentPath[currNode+1].y);
+                Vector3 end   = map.TileCoordToWorldCoord(currentPath[currNode+1].x, currentPath[currNode+1].y);
                 Debug.DrawLine(start, end);
                 currNode++;
             }
+        }
+    }
+    public void MoveNextTile()
+    {
+        int remainingMovement = moveSpeed;
+        while(remainingMovement > 0){
+            if (currentPath == null)
+            {
+                return;
+            }
+            //get cost form current tile to next tile
+            remainingMovement -= (int)map.CostToEnterTile(currentPath[0].x, currentPath[0].y, currentPath[1].x, currentPath[1].y);
 
+            // remove the old current node
+            currentPath.RemoveAt(0);
+
+            //set current tile on unit
+            
+            //move to next tile
+            tileX = currentPath[0].x;
+            tileY = currentPath[0].y;
+            transform.position = map.TileCoordToWorldCoord(tileX, tileY);
+            if (currentPath.Count == 1)
+            {
+                //next tile = the goal
+                currentPath = null;
+            }
+            
         }
     }
 }
