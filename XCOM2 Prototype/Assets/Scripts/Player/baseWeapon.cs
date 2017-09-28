e7b81adcb070a54ba332c58a2b22b72b7a6adcc0
JoeSnow0 <thomas.c.richards1@gmail.com>
2017-09-28 15:51:36 +0200

    added baseWeapon script with statistics for the unit's damage and accuracy.

---
+++ XCOM2 Prototype/Assets/Scripts/Player/baseWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class baseWeapon : MonoBehaviour {
+
+    [Header("Weapon Statistics")]
+    [Tooltip("Accuracy")]
+    [Range( 0f,  100f)]
+    public int weaponAim;
+    [Tooltip("Damage")]
+    [Range(1f, 5f)]
+    public int weaponDamage;
+}
