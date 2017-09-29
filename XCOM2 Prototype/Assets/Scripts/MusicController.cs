030c7bb1f2e15549e1ab67ae4afea96be70e0d79
UTB\dorpma <maxdorper@gmail.com>
2017-09-29 14:08:59 +0200

    Added StopSound and StopAllSound functions

--- XCOM2 Prototype/Assets/Scripts/MusicController.cs
+++ XCOM2 Prototype/Assets/Scripts/MusicController.cs
@@ -21,6 +21,7 @@ public class MusicController : MonoBehaviour {
 
     [Tooltip("Audio Source used:")]
     public AudioSource audioSource;
+    bool isPlaying = false;
 
     private void Awake()
     {
@@ -29,6 +30,8 @@ public class MusicController : MonoBehaviour {
 
         if (audioSource == null)
             audioSource = GetComponent<AudioSource>();
+
+        
     }
 
     // Use this for initialization
@@ -36,57 +39,73 @@ public class MusicController : MonoBehaviour {
 
         
 
-        SceneManager.GetSceneByBuildIndex(0);
+        //SceneManager.GetSceneByBuildIndex(0);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(0))
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(0) && !isPlaying)
         {
             PlaySound(0, true);
             PlaySound(1, true);
+            isPlaying = true;
+
         }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SceneManager.LoadScene(0);
-        }
-        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(3))
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(1) && !isPlaying)
         {
             PlaySound(2, true);
             PlaySound(3, true);
+            isPlaying = true;
         }
-        if (Input.GetKeyDown(KeyCode.D))
+
+        //debug load main menu scene
+        if (Input.GetKeyDown(KeyCode.P))
         {
-            PlaySound(2);
+            SceneManager.LoadScene(0);
+            
         }
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+
+
+            StopAllSound();
+            
+
+        }
 
     }
 
     public void PlaySound(int soundIndex, bool isLooping = false)
-    {       
-        //AudioSource aS = audioSource.GetComponent<AudioSource>();
-        //audioSource.Stop();
+    {              
         foreach( var audioSource in playingSources)
         {
-            //playingSources.Remove(audioSource);
+            
             if (!audioSource.isPlaying)
                 break;
             if (audioSource.clip.name == soundClip[soundIndex].name)
                 return;
         }
-
         audioSource = MundoSound.Play(soundClip[soundIndex], volumeMaster, Vector3.zero, isLooping, 0, transform);
-        playingSources.Add(audioSource);
-
-        //audioSource.loop = isLooping;
-
-        //audioSource.clip = soundClip[soundIndex];
-        //audioSource.Play();
+        playingSources.Add(audioSource);     
     }
 
-    void StopSound(int soundIndex)
+    public void StopSound(int soundIndex)
     {
+        foreach (var audioSource in playingSources)
+        {
+            if (audioSource.clip.name == soundClip[soundIndex].name)
+                audioSource.Stop();
+        }
+        isPlaying = false;
+    }
 
+    public void StopAllSound()
+    {
+        for (int i = 0; i < soundClip.Length; i++)
+        {
+            StopSound(i);
+
+        }
     }
 }
