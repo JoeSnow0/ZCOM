d99a50822041a4befa2a0535dda9776f7434df3b
Axel Fregler <axel_fregler@hotmail.com>
2017-10-02 15:02:32 +0200

    Gave all variables in BaseWeapon Public

--- XCOM2 Prototype/Assets/Scripts/Gameplay/BaseWeapon.cs
+++ XCOM2 Prototype/Assets/Scripts/Gameplay/BaseWeapon.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseWeapon : MonoBehaviour {
+public class BaseWeapon : MonoBehaviour
+{
 
 
-    [SerializeField] int value = 10;
-    [SerializeField] int baseAccuracy = 70;
-    [SerializeField] int rangeFalloff = 1;
-    [SerializeField] int weaponRange = 10;
-    [SerializeField] int baseDamage = 2;
-    [SerializeField] int numberOfDiceDamage = 1;
-    [SerializeField] int numberOfSideDamage = 3;
+    [SerializeField] public int value = 10;
+    [SerializeField] public int baseAccuracy = 70;
+    [SerializeField] public int rangeFalloff = 1;
+    [SerializeField] public int weaponRange = 10;
+    [SerializeField] public int baseDamage = 2;
+    [SerializeField] public int numberOfDiceDamage = 1;
+    [SerializeField] public int numberOfSideDamage = 3;
 
-    
+    //Multiple 
+        //Simple display (use if only single dices are used)
+        [Tooltip("Damage", (baseDamage+1), " + ", (baseDamage + numberOfSideDamage))]
+        //DnD display (use if multiple dices are used)          Damage 2+2D4
+        [Tooltip("Damage",baseDamage," + ",numberOfDiceDamage,"D",numberOfSideDamage)]
+        //Simplified display (use if multiple dices are used)   Damage 4-10
+        [Tooltip("Damage", (baseDamage + numberOfDiceDamage),"-",(baseDamage + (numberOfDiceDamage * numberOfSideDamage)))]
 }
 
 
