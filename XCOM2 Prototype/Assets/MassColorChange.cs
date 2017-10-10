7d2361241a1363ab2431931d7526e4c262359c5d
JoeSnow0 <thomas.c.richards1@gmail.com>
2017-10-10 11:09:25 +0200

    added color changer script. and button prefab. WIP

---
+++ XCOM2 Prototype/Assets/MassColorChange.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class MassColorChange : MonoBehaviour {
+
+    static List<MassColorChange> trackedObjects = new List<MassColorChange>();
+
+    public ScriptableObject ColorObject;
+
+	// Use this for initialization
+	void Start () {
+        trackedObjects.Add(this);
+	}
+
+    private void OnDestroy()
+    {
+        trackedObjects.Remove(this);
+    }
+
+    void ApplyColorChange()
+    {
+        Text c = GetComponent<Text>();
+        if (c)
+            c.color = ColorScriptableObject.primaryColor;
+
+
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (FindColorChange() != null)
+    //    {
+    //        Debug.Log("Hello World!");
+    //    }
+
+    //}
+}
---
+++ XCOM2 Prototype/Assets/Scripts/ColorScriptableObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[Serializable]
+
+[CreateAssetMenu(fileName = "ColorEditor", menuName = "Class/CreateColorEditor", order = 2)]
+public class ColorScriptableObject : ScriptableObject
+{
+    public Color primaryColor;
+    public Color secondaryColor;
+    public Color tertiaryColor;
+
+    public Sprite primarySprite;
+}
+
+
+
+
+
