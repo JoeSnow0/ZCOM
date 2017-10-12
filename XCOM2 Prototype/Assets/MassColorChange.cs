27a8349e77ec5f87e3ae3ef21e1e95cb6a1b2bc2
JoeSnow0 <thomas.c.richards1@gmail.com>
2017-10-12 10:04:48 +0200

    Fixed texture glitch in UI_Standard. Added a colorEditor which can be used to update all buttons graphics.

--- XCOM2 Prototype/Assets/MassColorChange.cs
+++ XCOM2 Prototype/Assets/MassColorChange.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class MassColorChange : MonoBehaviour {
 
     static List<MassColorChange> trackedObjects = new List<MassColorChange>();
 
-    public ScriptableObject ColorObject;
+    static ColorScriptableObject ScriptableColorObject;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        //Add ColorEditor to ScriptableColorObject
+        if (ScriptableColorObject == null)
+        {
+            ScriptableColorObject = AssetDatabase.LoadAssetAtPath<ColorScriptableObject>("Assets/Scriptable Object/ColorEditor.asset");
+        }
         trackedObjects.Add(this);
-	}
-
+    }
+    
     private void OnDestroy()
     {
         trackedObjects.Remove(this);
@@ -22,20 +29,58 @@ public class MassColorChange : MonoBehaviour {
 
     void ApplyColorChange()
     {
-        Text c = GetComponent<Text>();
-        if (c)
-            c.color = ColorScriptableObject.primaryColor;
+        //Update button Components
+        Button b = GetComponent<Button>();
+        if (b)
+        {
 
+            ColorBlock block = b.colors;
+            block.normalColor = ScriptableColorObject.normalColor;
+            block.highlightedColor = ScriptableColorObject.highlightColor;
+            block.pressedColor = ScriptableColorObject.pressedColor;
+            block.disabledColor = ScriptableColorObject.disabledColor;
+            block.colorMultiplier = ScriptableColorObject.colorMultiplier;
+            b.colors = block;
 
+            //return;
+        }
+
+        //Update outline Components
+        Outline o = GetComponent<Outline>();
+        if (o)
+        {
+            Vector2 shadow = o.effectDistance;
+            o.effectColor = ScriptableColorObject.effectColor;
+            shadow.x = ScriptableColorObject.effectDistanceX;
+            shadow.y = ScriptableColorObject.effectDistanceY;
+
+        }
+        //Update image Components
+        Image i = GetComponent<Image>();
+        if (i)
+        {
+            i.sprite = ScriptableColorObject.sourceImage;
+            i.color = ScriptableColorObject.imageColor;
+            i.material = ScriptableColorObject.imageMaterial;
+        }
+        //Update text Components
+        Text t = GetComponent<Text>();
+        if (t)
+        {
+            t.font = ScriptableColorObject.primaryFont;
+            t.color = ScriptableColorObject.textColor;
+        }
     }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (FindColorChange() != null)
-    //    {
-    //        Debug.Log("Hello World!");
-    //    }
-
-    //}
+    // Add a menu item named "Do Something" to MyMenu in the menu bar.
+    [MenuItem("Update Stuff/updateButtonColor")]
+    static void updateButtonColor()
+    {
+        foreach (MassColorChange itemsToChange in trackedObjects)
+        {
+            print("Hello");
+            itemsToChange.ApplyColorChange();
+        }
+           
+    }
 }
--- XCOM2 Prototype/Assets/Scripts/ColorScriptableObject.cs
+++ XCOM2 Prototype/Assets/Scripts/ColorScriptableObject.cs
@@ -8,11 +8,29 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ColorEditor", menuName = "Class/CreateColorEditor", order = 2)]
 public class ColorScriptableObject : ScriptableObject
 {
-    public Color primaryColor;
-    public Color secondaryColor;
-    public Color tertiaryColor;
+    [Header("Button Settings")]
+    public Color normalColor;
+    public Color highlightColor;
+    public Color pressedColor;
+    public Color disabledColor;
+    [RangeAttribute(1, 5)]
+    public int colorMultiplier;
 
-    public Sprite primarySprite;
+    [Header("Outline Settings")]
+    public Color effectColor;
+    public int effectDistanceX, effectDistanceY;
+
+    [Header("Image Settings")]
+    public Sprite sourceImage;
+    public Color imageColor;
+    public Material imageMaterial;
+
+    [Header("Text Settings")]
+    public Font primaryFont;
+    public Color textColor;
+
+
+
 }
 
 
