8b64c7f4eb0843aca5c5311d2a0e92b2c31d3bf8
dorpma <dorpma@3SE-SPEL-73>
2017-10-24 15:31:16 +0200

    Bug fix, nullrefs, prefab updated and much much more

--- XCOM2 Prototype/Assets/Scripts/Gameplay/TurnSystem.cs
+++ XCOM2 Prototype/Assets/Scripts/Gameplay/TurnSystem.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 
 public class TurnSystem : MonoBehaviour {
     [Header("Lists with all units")]
-    public UnitConfig[] allUnits;
-    public List<UnitConfig> playerUnits = new List<UnitConfig>();
-    public List<UnitConfig> enemyUnits = new List<UnitConfig>();
+    [HideInInspector]public UnitConfig[] allUnits;
+    [HideInInspector]public List<UnitConfig> playerUnits = new List<UnitConfig>();
+    [HideInInspector]public List<UnitConfig> enemyUnits = new List<UnitConfig>();
 
     //[Header("Actions")]
     [HideInInspector]
@@ -150,7 +150,7 @@ public class TurnSystem : MonoBehaviour {
                     MoveMarker(unitMarker, selectedUnit.transform.position);
                     MoveCameraToTarget(selectedUnit.transform.position, 0);
                     TileMap map = GetComponent<TileMap>();
-                    map.ChangeGridColor(selectedUnit.moveSpeed, selectedUnit.actionPoints.actions, selectedUnit);
+                    //map.ChangeGridColor(selectedUnit.moveSpeed, selectedUnit.actionPoints.actions, selectedUnit);
                 }
                 
             }
