ed384d17fc6fe5fb36f3b844267d49241819cb01
OliverDahlqvist <oliverdahlqvist97@gmail.com>
2017-11-08 11:19:45 +0100

    Objective now works

--- XCOM2 Prototype/Assets/Scripts/Gameplay/Objectives/ObjectiveSurvive.cs
+++ XCOM2 Prototype/Assets/Scripts/Gameplay/Objectives/ObjectiveSurvive.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectiveSurvive : Objective {
-    
+
 	void Start () {
         InitializeObjective();
         SetDescription("Survive " + mapConfig.turnSystem.maxTurns + " rounds");
-	}
+    }
 	
 	void Update () {
-		
+        if (mapConfig.turnSystem.hud.amountTurns > mapConfig.turnSystem.maxTurns)
+        {
+            SetState(1);
+        }
 	}
 }
